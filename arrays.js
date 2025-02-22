1087da0e252253d0c2752c0c4134b3c0f0977c97
NishatSiddiqui <siddiquin344@gmail.com>
2025-02-22 19:24:11 +0530

    update the elements in an array by index

--- arrays.js
+++ arrays.js
@@ -59,3 +59,13 @@ let rows = [];
 // ------------------------------------------
 
 
+// Step 24
+// In the last few steps, you learned all about working with arrays. Take a moment to review what you have learned.
+// Start by declaring a cities variable and initializing it as an array of the strings "London", "New York", and "Mumbai". Then log that variable to the console.
+// After logging, change the last element of cities to the string "Mexico City", then log the cities variable again.
+// When done correctly, you should see this output in the console.
+
+// let cities = ["London", "New York", "Mumbai"];
+// console.log(cities)
+// cities[cities[2]="Mexico City"];
+// console.log(cities)
