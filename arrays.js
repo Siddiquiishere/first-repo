d38ef0bd40b00be55d8a3a1298a54859be6cd300
NishatSiddiqui <siddiquin344@gmail.com>
2025-02-22 19:36:46 +0530

    push method

--- arrays.js
+++ arrays.js
@@ -8,7 +8,7 @@
 // Question
 // Declare a rows variable and assign it an empty array.
 
-let rows = [];
+// let rows = [];
 
 // ----------------------------
 
@@ -69,3 +69,18 @@ let rows = [];
 // console.log(cities)
 // cities[cities[2]="Mexico City"];
 // console.log(cities)
+// -----------------------------------------------
+
+// Step 26
+// A method in JavaScript is a function that's associated with certain values or objects. An example you've already encountered is the .log() method, which is part of the console object.
+// Arrays have their own methods, and the first you will explore is the .push() method. This allows you to "push" a value to the end of an array. Here is an example to add the number 12 to the end of an array:
+// Example Code
+// array.push(12);
+// Use .push() to add the string "freeCodeCamp" to the end of your rows array. Add this code before your console.log so you can see the change you made to your array.
+
+// let rows = ["Naomi", "Quincy", "CamperChan"];
+// rows.push("freeCodeCamp")
+
+// console.log(rows);
+
+// -----------------------------------------------
