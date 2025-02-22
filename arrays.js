968bbea29d82864a28ec102cf76b3b51150321d8
NishatSiddiqui <siddiquin344@gmail.com>
2025-02-22 17:09:23 +0530

    update valuse of elements in an array

--- arrays.js
+++ arrays.js
@@ -10,3 +10,35 @@
 // Declare a rows variable and assign it an empty array.
 
 let rows = [];
+
+// ----------------------------
+
+// Step 19
+// When an array holds values, or elements, those values are separated by commas. Here is an array that holds two strings:
+
+// Example Code
+// let array = ["first", "second"];
+// Change your rows declaration to be an array with the strings "Naomi", "Quincy", and "CamperChan". The order of values in an array is important, so follow that order. Remember that strings are case-sensitive.
+
+// let rows = ["Naomi", "Quincy", "CamperChan"];
+
+// -----------------------------------------------
+
+// Step 21
+// Arrays are special in that they are considered mutable. This means you can change the value at an index directly.
+
+// For example, this code would assign the number 25 to the second element in the array:
+
+// Example Code
+// let array = [1, 2, 3];
+// array[1] = 25;
+// console.log(array); // prints [1, 25, 3]
+// Update the third element of your rows array to be the number 10. Then print the rows array to your console.
+
+
+// rows = [1,2,3];
+// rows[2] = 10
+// console.log(rows)
+
+// -----------------------------------------------
+
