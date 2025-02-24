6e4fd57c52cc266ee446070eb10a2c67add926f8
NishatSiddiqui <siddiquin344@gmail.com>
2025-02-24 15:38:58 +0530

    func snd param

--- arrays.js
+++ arrays.js
@@ -216,3 +216,49 @@
 // for (let i = 0; i < count; i = i + 1) {
 //     rows.push(character.repeat(i));
 //   }
+
+
+// Step 55
+// Before moving on, take a moment to review how functions work.
+
+// Declare a function named addTwoNumbers. This function should take two arguments and return the sum of those two arguments.
+
+// Your function should not use hard-coded values. An example of a hard-coded function might be:
+
+// function sayName(firstName, lastName) {
+//     return "John Doe";
+//   }
+  
+//   sayName("Camper", "Cat");
+
+//   This function would return "John Doe" regardless of the arguments passed to the parameters firstName, and lastName, so "John Doe" is considered a hard-coded value.
+
+// Declare a sum variable and assign it the value of calling your addTwoNumbers function with 5 and 10 as the arguments. Log the sum variable to the console.
+
+// function addTwoNumbers(a,b){
+//     return a+b
+//   }
+//   const sum = addTwoNumbers(5,10)
+//   console.log(sum)
+
+
+
+// ------------------------------------------------------------
+
+// Step 57
+// Variables in JavaScript are available in a specific scope. In other words, where a variable is declared determines where in your code it can be used.
+
+// The first scope is the global scope. Variables that are declared outside of any "block" like a function or for loop are in the global scope. Your character, count, and rows variables are all in the global scope.
+
+// When a variable is in the global scope, a function can access it in its definition. Here is an example of a function using a global title variable:
+// const title = "Professor ";
+// function demo(name) {
+//   return title + name;
+// }
+// demo("Naomi")
+
+// This example would return "Professor Naomi". Update your padRow function to return the value of concatenating your character variable to the beginning of the name parameter.
+
+// function padRow(name) {
+//     return character+name
+//   }
