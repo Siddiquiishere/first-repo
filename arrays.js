abbbd3b7798c8e54e67fd9b7d6abb48b58c9b1d3
NishatSiddiqui <siddiquin344@gmail.com>
2025-02-24 16:05:28 +0530

    func function scope

--- arrays.js
+++ arrays.js
@@ -262,3 +262,26 @@
 // function padRow(name) {
 //     return character+name
 //   }
+
+
+// Step 59
+// Values returned out of a function are used by calling the function. You can use the function call directly as the value it returns, or capture the returned value in a variable. This way, you can use the value assigned to a locally scoped variable, outside the function it was created in.
+
+// function getName() {
+//     const name = "Camper cat";
+//     return name;
+//   }
+  
+//   console.log(getName()); // "Camper cat"
+  
+//   const capturedReturnValue = getName();
+//   console.log(capturedReturnValue); // "Camper cat"
+  
+//   console.log(name); // reference error
+
+//   To use your "Testing" value, return it out of the padRow function by updating your return statement to return only the test variable.
+
+//   function padRow(name) {
+//     const test = "Testing";  
+//     return test;
+//   }
