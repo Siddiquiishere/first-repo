727c05a0ea051f62835076b9a25ce7abad53b989
NishatSiddiqui <siddiquin344@gmail.com>
2025-02-26 14:46:08 +0530

    decrement operator

--- arrays.js
+++ arrays.js
@@ -402,3 +402,12 @@ if ("") {
 // while (done <= count) {
 //   done++;
 //   rows.push(padRow(done, count));
+// ---------------------------------------------------
+// Step 104
+// Because you are only subtracting one from i, you can use the decrement operator --.
+
+// Replace your subtraction assignment with the decrement operator.
+
+// for (let i = count; i > 0;  i--) {
+//   rows.push(padRow(i, count));
+// }
